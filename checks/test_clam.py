7e2a88ecdbae5acf78daa06ef304d64ba2b7d930
Maarten van Gompel <proycon@anaproy.nl>
2017-01-29 22:16:14 +0100

    removed subtests

--- checks/test_clam.py
+++ checks/test_clam.py
@@ -10,17 +10,17 @@ class TestClam(unittest.TestCase):
 
     def test_clam(self):
         for url,args in self.urls:
-            if ' ' in args:
-                argfile, username, password = args.split(' ')
-            else:
-                argfile = args
-                username = None
-                password = None
-            with open(argfile) as f:
-                args = json.load(f)
-            client = CLAMClient(url,username, password)
-            project = 'lamawebchecktest' + str("%034x" % random.getrandbits(128))
-            with self.subTest(i="CREATE " + url):
+            with self.subTest(i= url):
+                if ' ' in args:
+                    argfile, username, password = args.split(' ')
+                else:
+                    argfile = args
+                    username = None
+                    password = None
+                with open(argfile) as f:
+                    args = json.load(f)
+                client = CLAMClient(url,username, password)
+                project = 'lamawebchecktest' + str("%034x" % random.getrandbits(128))
                 client.create(project)
                 for file in args['upload']:
                     if not 'parameters' in file:
@@ -29,17 +29,14 @@ class TestClam(unittest.TestCase):
                         raise Exception("LamaWebCheck Configuration error: no file specified")
                     elif not os.path.exists(file['file']):
                         raise Exception("LamaWebCheck Configuration error: file " + file['file'] + " does not exist")
-                    with self.subTest(i="UPLOAD " + url + " <- " + file['file']):
-                        client.upload(project,file['inputtemplate'], file['file'], **file['parameters'])
-            if 'timeout' not in args:
-                args['timeout'] = 120
-            with self.subTest(i="START " + url):
+                    client.upload(project,file['inputtemplate'], file['file'], **file['parameters'])
+                if 'timeout' not in args:
+                    args['timeout'] = 120
                 if 'parameters' not in args:
                     args['parameters'] = {}
                 data = client.start(project, **args['parameters'])
                 if data.errors:
                     raise Exception("CLAM service returned an error upon start:" + str(data.errors))
-            with self.subTest(i="POLL " + url):
                 status = 0
                 i = 0
                 while status < 2: #while not done
@@ -50,13 +47,11 @@ class TestClam(unittest.TestCase):
                     data = client.get(project)
                     status = data.status
                     time.sleep(1)
-            if 'download' in args:
-                for file in args['download']:
-                    if 'file' not in file:
-                        raise Exception("LamaWebCheck Configuration error: no file specified")
-                    with self.subTest(i="DOWNLOAD " + url + " -> " + file['file']):
+                if 'download' in args:
+                    for file in args['download']:
+                        if 'file' not in file:
+                            raise Exception("LamaWebCheck Configuration error: no file specified")
                         client.download(project, file['file'], '/tmp/lamawebcheck_output')
-            with self.subTest(i="DELETE " + url):
                 client.delete(project)
 
 if __name__ == '__main__':
